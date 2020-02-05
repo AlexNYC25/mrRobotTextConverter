75107be88a354ec7f22917aca6c06f90282d6043
Alexis Montes <montes.alexis25@gmail.com>
2020-02-05 13:49:57 -0500

    changed to external router for index route

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const pug = require('pug');
+const indexRoute = require('./routes/indexRoute')
+const convertRoute = require('./routes/convertRoute')
 
 const app = express();
 const port = 3000;
@@ -7,13 +9,8 @@ const port = 3000;
 app.set('view engine', 'pug');
 app.set('views', __dirname.concat("/views"));
 
-app.get('/', function(req, res) {
-    res.render("index.pug");
-});
+app.use('/', indexRoute);
 
-app.get('/convert', function(req, res) {
-    let text = req.query.userText;
-    res.end(text)
-})
+
 
 app.listen(port);

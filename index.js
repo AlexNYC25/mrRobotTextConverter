d4f7f4f670cebd5c7cc6f720f0398b05765af4ff
Alexis Montes <montes.alexis25@gmail.com>
2020-02-07 20:32:03 -0500

    changed static files being served

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const pug = require('pug');
 const indexRoute = require('./routes/indexRoute')
-const convertRoute = require('./routes/convertRoute')
 
 const app = express();
 const port = 3000;
 
+app.use('/public', express.static(__dirname +  '/public'))
+//app.use('/node_modules', express.static(__dirname +  '/node_modules'))
+//app.use('/public/css', express.static('css'))
+
 app.set('view engine', 'pug');
+app.set('view options', { pretty: true });
 app.set('views', __dirname.concat("/views"));
 
 app.use('/', indexRoute);
